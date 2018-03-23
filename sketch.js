be0ccf66b57b754845c37fc11309732581377a45
Boortz <BrBoortz@ad.harman.com>
2018-03-23 13:48:54 -0400

    Bigger waves

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@
 // Original GIF: https://beesandbombs.tumblr.com/post/149654056864/cube-wave
 
 let angle = 0;
-let w = 24;
+let w = 16;
 let ma;
 let maxD;
 
@@ -17,29 +17,31 @@ function setup() {
   maxD = dist(0, 0, 300, 300);
 }
 
+function keepbetween(x){
+    if (x > 1) {
+        return 1;
+    }
+    if (x < -1) {
+        return -1;
+    }
+    return x;
+}
+
 function draw() {
   background(100);
   ortho(-400, 400, 400, -400, 0, 1000);
   rotateX(-ma);
   rotateY(-QUARTER_PI)﻿
-
-  function keepbetween(x){
-      if (x > 1) {
-          return 1;
-      }
-      if (x < -1) {
-          return -1;
-      }
-      return x;
-  }
-
+  //if (angle < -7|| angle > 7){
+    //  angle = 0;
+    //}
   for (let z = 0; z < height; z += w) {
     for (let x = 0; x < width; x += w) {
       push();
       let d = dist(x, z, width / 2, height / 2);
       let offset = map(d, 0, maxD, -PI, PI);
       let a = angle + offset;
-      let h = floor(map(keepbetween(tan(a)), -1, 1, 100, 300));
+      let h = floor(map(keepbetween(sin(a)), -1, 1, 0, 500));
       translate(x - width / 2, 0, z - height / 2);
       normalMaterial();
       box(w, h, w);
@@ -48,5 +50,7 @@ function draw() {
     }
   }
 
-  angle -= 0.1;
+
+    angle -= 0.1;
+
 }
